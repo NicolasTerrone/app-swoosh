fec3ef41f7f11352c76d4de5b3ef469d9478af10
Nicolas TERRONE <nicolasterrone@gmail.com>
2019-01-17 19:57:02 +0100

    Refactoring code and added missing segues

---
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,34 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Nicolas Terrone on 17/01/2019.
+//  Copyright © 2019 Nicolas Terrone. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onNextTapped(_ sender: Any) {
+        performSegue(withIdentifier: "skillVCSegue", sender: self)
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
